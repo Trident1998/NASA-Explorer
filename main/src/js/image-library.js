391aa5533a125cb12b0284832a68386f592e1643
Dmytro <dima4994@gmail.com>
2024-03-31 18:44:50 +0300

    complited image-library

--- main/src/js/image-library.js
+++ main/src/js/image-library.js
@@ -1,21 +1,54 @@
-import { getTestData } from "./external-services.js";
+import { loadHeaderFooter } from "./load-header.js";
+import { initializeMenu, setActiveByText } from "./menu.js";
+import { convertToJson } from "./external-services.js";
+import { imageLibraryCardTemplate } from "./json-hendler.js"
 
+const baseURL = "https://images-api.nasa.gov"
 
-getTestData();
+document.addEventListener("DOMContentLoaded", async () => {
+    await loadHeaderFooter();
 
-document.getElementById("searchForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Get form values
-    const description = document.getElementById("description").value;
-
-    // Perform search with the form values
-    search(description);
+    initializeMenu();
+    setActiveByText('Image Library');
 });
 
-function search(description) {
-    // Perform search logic here
-    // For demonstration purposes, let's just display the search parameters
-    const searchResultsDiv = document.getElementById("searchResults");
-    searchResultsDiv.innerHTML = `<p>Searching with Description: ${description}, Start Year: ss</p>`;
+document.getElementById("searchForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const description = document.getElementById("description").value;
+
+    try {
+        searchImage(description);
+    } catch {
+        searchNotFound(description);
+    }
+});
+
+function searchNotFound(description) {
+    const message = document.createElement('p');
+    message.textContent = `No results found for "${description}".`;
+    message.style.color = 'red';
+    message.style.fontWeight = 'bold';
+    
+    document.querySelector(".image-list").innerHTML = '';
+    const searchResultsContainer = document.querySelector('.error-message');
+    searchResultsContainer.appendChild(message);
 }
+
+async function searchImage(searchTeerm) {
+    document.querySelector(".error-message").innerHTML = '';
+    const element = document.querySelector(".image-list");
+    const response = await fetch(baseURL + `/search?q=${searchTeerm}`);
+    const data = await convertToJson(response);
+    console.log(data.collection.items)
+  
+    const list = data.collection.items.map(imageLibraryCardTemplate).join("");
+  
+    if (data.collection.items != 0) {
+        console.log(list);
+        element.innerHTML = list;
+    } else {
+        searchNotFound(searchTeerm);
+    }
+  }
+
+  searchImage('Supernova');
